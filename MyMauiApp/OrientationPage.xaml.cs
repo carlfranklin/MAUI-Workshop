7ceb940adfe575d70a223f4a9dc2acb604efd1e7
Carl Franklin <carl@franklins.net>
2023-05-15 16:49:36 +0200

    Added Wim Van Der Schueren's excellent code for determining Portrait or Landscape code. Thanks, Wim!

--- MyMauiApp/OrientationPage.xaml.cs
+++ MyMauiApp/OrientationPage.xaml.cs
@@ -32,18 +32,32 @@ public partial class OrientationPage : ContentPage
     {
         // Update UI Label with orientation state
         OrientationLabel.TextColor = Colors.Green;
-        if (IsLandscape(e.Reading))
+        if (!IsPortrait(e.Reading))
             OrientationLabel.Text = "Landscape";
         else
             OrientationLabel.Text = "Portrait";
     }
 
-    private bool IsLandscape(OrientationSensorData orientationSensorData)
+    private bool IsPortrait(OrientationSensorData orientationSensorData)
     {
-        var value = orientationSensorData.Orientation;
-        if (value.Y >= 0.00)
-            return true;
+        var q = orientationSensorData.Orientation;
+
+        var sinp = +2.0 * (q.W * q.Y - q.Z * q.X);
+        double pitch;
+        if (Math.Abs(sinp) >= 1)
+            pitch = Math.CopySign(Math.PI / 2, sinp); // use 90 degrees if out of range
         else
-            return false;
+            pitch = Math.Asin(sinp);
+
+        // Convert pitch to degrees for easier understanding
+        pitch = pitch * 180.0 / Math.PI;
+
+        // The device is considered in portrait mode if it's tilted horizontally, meaning the pitch
+        // is closer to 0 or 180.
+        if ((pitch <= 45 && pitch >= -45) || pitch >= 135 || pitch <= -135)
+            return true;  // Portrait
+        else
+            return false; // Landscape
     }
+
 }
